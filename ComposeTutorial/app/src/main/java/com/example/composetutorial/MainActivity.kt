0dad8dcb0fef52fc8fad87314af4a7514f85a49f
Reo Ando <ando.reo420@gmail.com>
2021-03-17 15:41:19 +0900

    TextViewの追加

--- ComposeTutorial/app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ ComposeTutorial/app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -2,9 +2,14 @@ package com.example.composetutorial
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent { 
+            Text(text = "Hello, Android!")
+        }
     }
 }
