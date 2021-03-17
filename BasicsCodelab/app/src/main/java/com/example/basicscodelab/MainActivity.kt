cd7a1efee9e3ff03e20dc3543f833995681e8865
Reo Ando <ando.reo420@gmail.com>
2021-03-17 21:25:54 +0900

    内部状態をComposableに追加

--- BasicsCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ BasicsCodelab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Button
+import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,27 +24,53 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(color = Color.Green) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(16.dp))
+fun MyApp(content: @Composable () -> Unit) {
+    BasicsCodelabTheme {
+        content()
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun MyScreenContent(names: List<String> = listOf("Android", "ios")) {
+    Column {
+        val counterState = remember { mutableStateOf(0) }
+        for (name in names) {
+            Surface(color = Color.Green) {
+                Greeting(name = name)
+                Divider()
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Counter(count = counterState.value, updateCount = {
+            counterState.value = it
+        })
+    }
+}
+
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button(onClick = { updateCount(count + 1) }) {
+        Text("I've been clicked $count times")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!", modifier = Modifier.padding(16.dp))
+}
+
+@Preview("MyScreen preview")
 @Composable
 fun DefaultPreview() {
-    BasicsCodelabTheme {
-        Greeting("Android")
+    MyApp {
+        MyScreenContent()
     }
 }
