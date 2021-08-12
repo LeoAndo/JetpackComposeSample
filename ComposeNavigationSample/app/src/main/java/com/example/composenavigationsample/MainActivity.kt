f56f4fd8499d0cd19d00b45cb3cd900a0635025c
Leo Ando <ando.reo420@gmail.com>
2021-08-12 19:19:22 +0900

    apply compose nav animation animation

--- ComposeNavigationSample/app/src/main/java/com/example/composenavigationsample/MainActivity.kt
+++ ComposeNavigationSample/app/src/main/java/com/example/composenavigationsample/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.composenavigationsample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
 import com.example.composenavigationsample.ui.theme.ComposeNavigationSampleTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -15,6 +17,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@ExperimentalAnimationApi
 @Composable
 fun MyApp() {
     ComposeNavigationSampleTheme {
