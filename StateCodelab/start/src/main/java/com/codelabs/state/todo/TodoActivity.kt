5bcfb8fec73dba027a3d068d42a20f881573f08d
LeoAndo <leo.777.info@gmail.com>
2021-11-23 14:43:05 +0900

    Pass the state down

--- StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
+++ StateCodelab/start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
@@ -23,6 +23,8 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import com.codelabs.state.ui.StateCodelabTheme
 
 class TodoActivity : AppCompatActivity() {
@@ -42,7 +44,8 @@ class TodoActivity : AppCompatActivity() {
 
     @Composable
     private fun TodoActivityScreen(todoViewModel: TodoViewModel) {
-        val item = listOf<TodoItem>()
+        // val item = listOf<TodoItem>()
+        val item: List<TodoItem> by todoViewModel.todoItems.observeAsState(listOf())
         TodoScreen(
             items = item,
             onAddItem = {
